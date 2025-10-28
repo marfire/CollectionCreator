9bd300bcafd7bbeb4f075f852fe2ccbbc44c914e
Kevin Christopher Henry <k@severian.com>
2025-10-27 22:39:28 -0700

    Change the default date format to %Y-%m-%d
    This aligns alphabetical sort order with chronological order, unlike the previous format.

--- CollectionCreator.lrplugin/Utils.lua
+++ CollectionCreator.lrplugin/Utils.lua
@@ -51,12 +51,6 @@ function Utils.formatNumber(num)
     return result
 end
 
--- Formats today's date as a string.
-function Utils.formatDate()
-    -- %e inserts an extra space instead of a 0, so we need to remove it.
-    return os.date("%B %e, %Y"):gsub("%s+", " ")
-end
-
 -- Compares two objects by their title field.
 function Utils.titleCompare(a, b)
     return a.title < b.title
